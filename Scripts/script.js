de906be316405778eb3c8a8d095c1198e1ca4565
xn--diego lamaro-dcb <diego.lamarao92@outlook.com>
2018-02-21 00:53:00 +0000

    v1.1.0

--- Scripts/script.js
+++ Scripts/script.js
@@ -14,10 +14,18 @@ angular.module("app", [])
     .controller('CategoryController', CategoryController)
     .controller('IndexController', IndexController);
 
-function TaskController() {
+function TaskController($http) {
     let self = this;
     self.status = stats;
-    self.tarefas = taks;
+    self.tarefas = [];
+    console.log(self.tarefas);
+    $http.get('http://taskmanager-api.azurewebsites.net/api/Taskmanager')
+      .then(function(result) {
+        result.data.forEach(t => {
+            self.tarefas.push(t);
+        });
+    });
+    console.log(self.tarefas);
     self.checkInput = checkInput;
     self.cadTask = cadTask;
     self.removeTask = removeTask;
@@ -104,22 +112,6 @@ function checkInput(tInput, tlist, cInput) {
         $('.btn-group').css('border', '2px orange solid');
     }
 }
-
-let taks = [
-    { nome: 'Varrer o quarto', categoria: 'Casa', feito: false },
-    { nome: 'Lavar a louça', categoria: 'Casa', feito: true },
-    { nome: 'Lavar as roupas', categoria: 'Casa', feito: false },
-    { nome: 'Consertar a porta', categoria: 'Casa', feito: false },
-    { nome: 'Terminar o relatório', categoria: 'Trabalho', feito: false },
-    { nome: 'Estudar para a próxima reunião', categoria: 'Trabalho', feito: false },
-    { nome: 'Enviar email sobre o problema', categoria: 'Trabalho', feito: false },
-    { nome: 'Levar pó de café', categoria: 'Trabalho', feito: true },
-    { nome: 'Comprar canetas novas', categoria: 'Trabalho', feito: true },
-    { nome: 'Estudar Etapa 1', categoria: 'Faculdade', feito: true },
-    { nome: 'Estudar Etapa 2', categoria: 'Faculdade', feito: true },
-    { nome: 'Estudar Etapa 3', categoria: 'Faculdade', feito: false },
-    { nome: 'Fazer TP1', categoria: 'Faculdade', feito: false }
-];
 
 let stats = [
     { nome: false },
