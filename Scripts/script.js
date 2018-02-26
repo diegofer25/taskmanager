8aa91be248682a81d8cac106be45d0f2ca04cf7d
xn--diego lamaro-dcb <diego.lamarao92@outlook.com>
2018-02-26 15:50:33 +0000

    v1.0.5

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,12 +1,14 @@
 let sessionName = sessionStorage.getItem('sessionName');
 let sessionId = sessionStorage.getItem('sessionId');
-let page = window.location.pathname;
+let sessionPhoto = sessionStorage.getItem('sessionPhoto');
 
 $(document).ready(function () {
-    let time = setInterval(() => {
+    if( !(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))) {
+        let time = setInterval(() => {
         $('#menu-toggle').trigger('click');
         clearInterval(time);
-    }, 300);
+        }, 300);
+    }
     $("#menu-toggle").click(function (e) {
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
@@ -21,12 +23,28 @@ angular.module("app", [])
     
 function RouterController(){
     let self = this;
+    self.year = (new Date()).getFullYear();
+    self.titleIndex = "Login";
+    self.titleRegister = "Cadastro";
+    self.titleSessionError = "Seção expirada";
+    self.titleHome = "Home";
+    self.titleTasks = "Gerenciar Tarefas";
+    self.titleCats = "Gerenciar Categorias";
     self.checkSession = checkSession;
     self.index = index;
+    self.register = register;
+    self.sessionError = sessionError;
+    self.HomeIndex = HomeIndex;
+    self.CategoryIndex = CategoryIndex;
+    self.TaskIndex = TaskIndex;
+    self.exit = exit;
     
     function checkSession(check){
-        if(sessionName && sessionId){
-            
+        if(sessionName && sessionId && sessionName != "undefined" && sessionId != "undefined"){
+            let path = window.location.pathname;
+            if( path == '/' || path == '/register.html' || path == '/sessionerror.html'){
+                window.location.assign("../WebView/HomeIndex.html");
+            }
         }else if(check){
             window.location.assign("../sessionerror.html");
         }
@@ -35,47 +53,120 @@ function RouterController(){
     function index(){
         window.location.assign("../index.html");
     }
+    function register(){
+        window.location.assign("../register.html");
+    }
+    function sessionError(){
+        window.location.assign("../sessionerror.html");
+    }
+    function HomeIndex(){
+        window.location.assign("../WebView/HomeIndex.html");
+    }
+    function CategoryIndex(){
+        window.location.assign("../WebView/CategoryIndex.html");
+    }
+    function TaskIndex(){
+        window.location.assign("../WebView/TaskIndex.html");
+    }
+    function exit(){
+        sessionStorage.setItem('sessionName', "undefined");
+        sessionStorage.setItem('sessionId', "undefined");
+        window.location.assign("../index.html");
+    }
 }
 
-function UserController($http, $window) {
+function UserController($http) {
     let self = this;
     self.name = sessionName;
     self.id = sessionId;
+    self.foto = sessionPhoto;
     self.cadUser = cadUser;
-
-    function cadUser(name, email, password, fbfoto) {
-        if (nome && email && password) {
-            document.getElementById("overlay").style.display = "block";
+    self.loginUser = loginUser;
+    
+    function cadUser(name, email, senha, fbfoto) {
+        if (senhaValida(senha)) {
+            lockScreen();
             let foto = "../images/profile.png";
             if (fbfoto) {
                 foto = fbfoto;
             }
             $http({
-                "method": "POST",
-                "async": true,
-                "crossDomain": true,
-                "url": "http://taskmanager-api.azurewebsites.net/api/User",
-                "headers": {
+                  "method": "POST",
+                  "async": true,
+                  "crossDomain": true,
+                  "url": "https://taskmanager-api.azurewebsites.net/api/User",
+                  "headers": {
                     "content-type": "application/json"
-                },
+                  },
                 "processData": false,
                 "data": {
                     "nome": name,
                     "email": email,
-                    "senha": password,
+                    "senha": senha,
                     "foto": foto
                 }
-            }
-            ).then(function (response) {
-                sessionStorage.setItem('sessionName', response.data.nome);
-                sessionStorage.setItem('sessionId', response.data.id);
-                $window.location.href = 'WebView/HomeIndex.html';
+            }).then(function (response) {
+                let user = response.data;
+                let firstName = user.nome.split(" ");
+                sessionStorage.setItem('sessionName', firstName[0]);
+                sessionStorage.setItem('sessionId', user.id);
+                sessionStorage.setItem('sessionPhoto', user.foto);
+                window.location.assign('WebView/HomeIndex.html');
             }).catch(function (response) {
-                console.log("Put Fail: " + response.data);
-                document.getElementById("overlay").style.display = "none";
-                $("#registerstatus").attr("class","btn btn-warning col-12");
-                $("#registerstatus").text(' E-mail já cadastrado no sistema');
-                $("#registerstatus>span").attr("class","fa fa-warning");
+                unlockScreen();
+                if(fbfoto){
+                    unlockScreen();
+                    showWaning("#loginstatus", 'Erro ao realizar Login via Facebook');
+                }else if(response.status == "500"){
+                    showWaning("#registerstatus", 'Erro na comunicação com o servidor');
+                }else{
+                    showWaning("#registerstatus", 'E-mail já cadastrado no sistema');
+                }
+            });
+        }else{
+            unlockScreen();
+            showWaning("#registerstatus", 'Sua senha não obecede a regra');
+        }
+    }
+    
+    function senhaValida(senha){
+        let ret = true;
+        if(senha.length < 6 || senha.length > 32){
+            ret = false;
+        }
+        return ret;
+    }
+    
+    function loginUser(email, senha, fbInfo){
+        if(email && senha){
+            lockScreen();
+            $http({
+              "method": "GET",
+              "async": true,
+              "crossDomain": true,
+              "url": `https://taskmanager-api.azurewebsites.net/api/User?email=${email}&senha=${senha}`,
+              "headers": {
+                "cache-control": "no-cache",
+              }
+            }).then(function (response) {
+              let user = response.data;
+              let firstName = user.nome.split(" ");
+              sessionStorage.setItem('sessionName', firstName[0]);
+              sessionStorage.setItem('sessionId', user.id);
+              sessionStorage.setItem('sessionPhoto', user.foto);
+              window.location.assign('WebView/HomeIndex.html');
+            })
+            .catch(function(response){
+                if(fbInfo){
+                    if(fbInfo.email){
+                        cadUser(fbInfo.name, fbInfo.email, fbInfo.id, fbInfo.picture.data.url);
+                    }else{
+                        cadUser(fbInfo.name, fbInfo.id, fbInfo.id, fbInfo.picture.data.url);
+                    }
+                }else{
+                    unlockScreen();
+                    showWaning("#loginstatus", 'E-mail ou senha inválido');
+                }
             });
         }
     }
@@ -85,7 +176,7 @@ function TaskController($http) {
     let self = this;
     self.status = stats;
     self.tarefas = [];
-    $http.get('http://taskmanager-api.azurewebsites.net/api/Task')
+    $http.get(`https://taskmanager-api.azurewebsites.net/api/Task/${sessionId}`)
       .then(function(result) {
         result.data.forEach(t => {
             self.tarefas.push(t);
@@ -97,16 +188,16 @@ function TaskController($http) {
     self.showdash = showdash;
     self.flagTask = flagTask;
     
-    function cadTask(tInput, cInput, tlist) {
+    function cadTask(tInput, cInput, tlist, uid) {
         let info = $('#button-info');
         let submit = $('#button-submit');
         if (tInput && tlist) {
-            
+            lockScreen();
             $http({
               "method": "POST",
               "async": true,
               "crossDomain": true,
-              "url": "http://taskmanager-api.azurewebsites.net/api/task",
+              "url": "https://taskmanager-api.azurewebsites.net/api/task",
               "headers": {
                 "content-type": "application/json",
               },
@@ -114,14 +205,19 @@ function TaskController($http) {
               "data": {
                     nome: tInput,
                     categoria: cInput,
-                    feito: false
+                    feito: false,
+                    userid: uid
                   }
             }).then(function (response) {
+              unlockScreen();
               self.categorias.push({ nome: cat });
-              console.log("Post Success");
+              submit.attr('class', 'd-none');
+              $('.form-inline').css('border', 'none');
+              info.attr('class', 'btn btn-success col-sm-12');
+              info.html('<span class="fa fa-check-circle"> Tarefa cadastrada com sucesso</span>');
             })
             .catch(function(){
-                console.log("Post Fail");
+                unlockScreen();
             });
             
             tlist.push({
@@ -129,53 +225,48 @@ function TaskController($http) {
                 categoria: cInput,
                 feito: false
             });
-            submit.attr('class', 'd-none');
-            $('.form-inline').css('border', 'none');
-            info.attr('class', 'btn btn-success col-sm-12');
-            info.html('<span class="fa fa-check-circle"> Tarefa cadastrada com sucesso</span>');
         }
     }
     
     function removeTask(tlist, key, id) {
         let info = $('#button-info');
         if (confirm(`Deseja mesmo excluir a tarefa "${tlist[key].nome}"`)) {
-            
+            lockScreen();
             $http({
               "method": "DELETE",
               "async": true,
               "crossDomain": true,
-              "url": `http://taskmanager-api.azurewebsites.net/api/task/${id}`,
+              "url": `https://taskmanager-api.azurewebsites.net/api/task/${id}`,
             }).then(function (response) {
-            	console.log("Delete Success");
+            	unlockScreen();
                 tlist.splice(key, 1);
+                info.attr('class', 'btn btn-danger col-sm-12');
+                info.html('<span class="fa fa-check-circle"> Categoria excluida com sucesso</span>');
             })
             .catch(function(){
-            	console.log("Delete Fail");
+                unlockScreen();
             });
-            
-            info.attr('class', 'btn btn-danger col-sm-12');
-            info.html('<span class="fa fa-check-circle"> Categoria excluida com sucesso</span>');
         }
     }
     
     function flagTask(isdo, key, id){
-        
+        lockScreen();
         $http({
           "method": "PUT",
           "async": true,
           "crossDomain": true,
-          "url": `http://taskmanager-api.azurewebsites.net/api/task/${id}`,
+          "url": `https://taskmanager-api.azurewebsites.net/api/task/${id}`,
           "headers": {
             "content-type": "application/json",
           },
           "processData": false,
           "data": {feito:isdo}
         }).then(function (response) {
+            unlockScreen();
             self.tarefas[key].feito = isdo;
-        	console.log("Put Success");
         })
         .catch(function(){
-        	console.log("Put Fail");
+            unlockScreen();
         });
     }
 }
@@ -183,7 +274,7 @@ function TaskController($http) {
 function CategoryController($http) {
     let self = this;
     self.categorias = [];
-    $http.get('http://taskmanager-api.azurewebsites.net/api/Category')
+    $http.get(`https://taskmanager-api.azurewebsites.net/api/Category/${sessionId}`)
       .then(function(result) {
         result.data.forEach(c => {
             self.categorias.push(c);
@@ -194,31 +285,33 @@ function CategoryController($http) {
     self.removeCategoria = removeCategoria;
     self.eraseInput = eraseInput;
     
-    function addCategoria(cat, categorias) {
+    function addCategoria(cat, categorias, uid) {
         let but = $('#button-status');
         if (cat && categorias) {
-            
+            lockScreen();
             $http({
               "method": "POST",
               "async": true,
               "crossDomain": true,
-              "url": "http://taskmanager-api.azurewebsites.net/api/category",
+              "url": "https://taskmanager-api.azurewebsites.net/api/category",
               "headers": {
                 "content-type": "application/json",
               },
               "processData": false,
-              "data": { nome : cat }
+              "data": { 
+                  nome : cat,
+                  userid: uid
+                  }
             }).then(function (response) {
+              unlockScreen();
               self.categorias.push({ nome: cat });
-              console.log("Post Success");
+              but.attr('class', 'btn btn-success col-sm-8 mb-2 mb-sm-0');
+              but.html('<span class="fa fa-check-circle"> Categoria cadastrada com sucesso</span>');
+              $('#button-submit').attr('class', 'd-none');
             })
             .catch(function(){
-                console.log("Post Fail");
+                unlockScreen();
             });
-            
-            but.attr('class', 'btn btn-success col-sm-8 mb-2 mb-sm-0');
-            but.html('<span class="fa fa-check-circle"> Categoria cadastrada com sucesso</span>');
-            $('#button-submit').attr('class', 'd-none');
         }
     }
 
@@ -226,24 +319,23 @@ function CategoryController($http) {
         let but = $('#button-status');
         let status = $('#status-remove');
         if (confirm(`Deseja mesmo excluir a categoria "${list[key].nome}"`)) {
-            
+            lockScreen();
             $http({
               "method": "DELETE",
               "async": true,
               "crossDomain": true,
-              "url": `http://taskmanager-api.azurewebsites.net/api/category/${id}`,
+              "url": `https://taskmanager-api.azurewebsites.net/api/category/${id}`,
             }).then(function (response) {
-            	console.log("Delete Success");
+                unlockScreen();
                 list.splice(key, 1);
+                but.html('<span class="fa fa-info-circle"> Cadastre aqui uma nova Categoria</span>');
+                but.attr('class', 'btn btn-info col-sm-8 mb-2 mb-sm-0');
+                status.attr('class', 'btn btn-danger col-sm-12');
+                status.html('<span class="fa fa-check-circle"> Categoria excluida com sucesso</span>');
             })
             .catch(function(){
-            	console.log("Delete Fail");
+                unlockScreen();
             });
-            
-            but.html('<span class="fa fa-info-circle"> Cadastre aqui uma nova Categoria</span>');
-            but.attr('class', 'btn btn-info col-sm-8 mb-2 mb-sm-0');
-            status.attr('class', 'btn btn-danger col-sm-12');
-            status.html('<span class="fa fa-check-circle"> Categoria excluida com sucesso</span>');
         }
     }
 }
@@ -371,5 +463,47 @@ function showdash(taks, cats) {
             labels: [`${parseInt((concluidas / total) * 100)}%`, `${parseInt((pendentes / total) * 100)}%`],
             series: [(concluidas / total) * 100, (pendentes / total) * 100]});
         }
+    });
+}
+
+function lockScreen(){
+    $("#overlay").css("display", "block");
+}
+
+function unlockScreen(){
+    $("#overlay").css("display", "none");
+}
+
+function showWaning(elem, msg){
+    $(elem).attr("class","btn btn-warning col-12");
+    $(elem).text(msg);
+    $(elem+">span").attr("class","fa fa-warning");
+}
+
+let token = "";
+
+function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      token = response.authResponse.accessToken;
+      statusChangeCallback(response);
+    });
+  }
+  
+  function statusChangeCallback(response) {
+    if (response.status === 'connected') {
+      fblogin();
+    }
+  }
+  
+  function fblogin() {
+    FB.api('/me?fields=id,name,picture,email', function(response) {
+        if(response.email){
+            angular.element(document.getElementById('page-content-wrapper')).scope()
+            .User.loginUser(response.email, response.id, response);
+        }else{
+            angular.element(document.getElementById('page-content-wrapper')).scope()
+            .User.loginUser(response.id, response.id, response);
+        }
+      
     });
 }
